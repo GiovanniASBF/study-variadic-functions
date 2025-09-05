91ed72cace601cb9960c4fe071a5630449b6bdf5
GiovanniBernardesFigueiredo <giovannibernardesfigueiredo@gmail.com>
2025-09-05 20:16:00 -0300

    add a function that will sum an undefined number of ints

---
+++ src/variadic_sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdarg.h>
+/*
+The function receives a fix argument 'count', that tell us how many 
+numbers we should expect in the variadic arguments list.
+*/
+int variadic_sum(int count, ...)
+{
+
+}
