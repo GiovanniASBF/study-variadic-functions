4d3fbb3b2216dedf0b58a73001c8e6058cee5e4c
GiovanniBernardesFigueiredo <giovannibernardesfigueiredo@gmail.com>
2025-09-05 20:33:18 -0300

    all the steps to deal with variadic arguments

--- src/variadic_sum.c
+++ src/variadic_sum.c
@@ -6,5 +6,24 @@ numbers we should expect in the variadic arguments list.
 */
 int variadic_sum(int count, ...)
 {
+    // 1. Declare an arguments list
+    va_list args;
+    int number;
+    int total;
 
+    // 2. Initialize the list. 'args' now points to the first variadic argument
+    // that comes AFTER the argument 'count'.
+    va_start(args, count);
+    total = 0;
+    // 3. Iterate over each argument in the list.
+    for (int i = 0; i < count; i++)
+    {
+        // va_arg reads the value of the current argument as an 'int' and
+        // advance the 'args' pointer to the next argument.
+        number = va_arg(args, int);
+        total += number;
+    }
+    // 4. Clean the arguments list. 
+    va_end(args);
+    return (total);
 }
